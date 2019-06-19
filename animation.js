89817f7da3945432924a4ffca9e36d86668c40e4
Sophie <sophie.k.mclean@btinternet.com>
2019-06-19 20:05:04 +0100

    added event listner so that greensock animation would run

--- animation.js
+++ animation.js
@@ -1,5 +1,8 @@
-var tl = new TimelineMax({repeat: 30, delay: 5});
+document.addEventListener('DOMContentLoaded', function() {
+
+var tl = new TimelineMax({repeat: 30, delay: 3});
 
 tl.to("#building", 2, {opacity: 1});
 tl.to("#what", 2, {opacity: 1});
 tl.to("#matters", 2, {opacity: 1});
+});
