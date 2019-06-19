eb58562da8a7be1c14250043b083b013fbceeb74
Sophie <sophie.k.mclean@btinternet.com>
2019-06-19 16:11:48 +0100

    added fade in animation to text

--- animation.js
+++ animation.js
@@ -1,1 +1,5 @@
-TweenMax.to(".text", 5, {left:600});
+var tl = new TimelineMax({repeat: 30, delay: 5});
+
+tl.to("#building", 2, {opacity: 1});
+tl.to("#what", 2, {opacity: 1});
+tl.to("#matters", 2, {opacity: 1});
