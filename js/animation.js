7dbbaaf2c1149a1bb0995dbdc13da8fa27987af0
Sophie <sophie.k.mclean@btinternet.com>
2019-06-20 18:33:12 +0100

    add new images and animation to banner so tesla drives off screen

--- js/animation.js
+++ js/animation.js
@@ -5,4 +5,7 @@ var tl = new TimelineMax({repeat: 30, delay: 0});
 tl.to("#building", 2.5, {opacity: 1});
 tl.to("#what", 2.5, {opacity: 1});
 tl.to("#matters", 2.5, {opacity: 1});
+tl.to(".text", 2.5, {opacity: 0});
+tl.to("#car", 5, {x: -1200});
+tl.to(".wheels", 5, {rotation: -220}, '-=5.1');
 });
