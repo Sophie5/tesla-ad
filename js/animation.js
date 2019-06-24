220beb35f66d4c7d42c5a268bee6ef5a6540c5b4
Sophie <sophie.k.mclean@btinternet.com>
2019-06-24 23:17:06 +0100

    added indentation and added code for each sized creative into seperate folders

--- js/animation.js
+++ js/animation.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
+  var tl = new TimelineMax({repeat: 30, delay: 0});
 
-var tl = new TimelineMax({repeat: 30, delay: 0});
-
-tl.to("#building", 2.5, {opacity: 1});
-tl.to("#what", 2.5, {opacity: 1});
-tl.to("#matters", 2.5, {opacity: 1});
-tl.to(".text", 2.5, {opacity: 0});
-tl.to("#car", 5, {x: -1200});
-tl.to(".wheels",3, {rotation: -600}, '-=5.1');
-tl.to("#logo", 3, {x: -89, y: 35, scale: 1.7}, "-=4" );
-tl.to("#slogan", 3, {opacity: 1});
+  tl.to("#building", 2.5, {opacity: 1});
+  tl.to("#what", 2.5, {opacity: 1});
+  tl.to("#matters", 2.5, {opacity: 1});
+  tl.to(".text", 2.5, {opacity: 0});
+  tl.to("#car", 5, {x: -1200});
+  tl.to(".wheels",3, {rotation: -600}, '-=5.1');
+  tl.to("#logo", 3, {x: -89, y: 35, scale: 1.7}, "-=4" );
+  tl.to("#slogan", 3, {opacity: 1});
 });
